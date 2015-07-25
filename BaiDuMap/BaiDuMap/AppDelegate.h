e5fcff58b4feda49cd83b14a0ffe10e2890a5493
lanou3g <lanou3g@lanou3gdeMac-mini-719.local>
2015-07-25 16:10:56 +0800

    百度地图初始化

---
+++ BaiDuMap/BaiDuMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiDuMap
+//
+//  Created by lanou3g on 15-7-25.
+//  Copyright (c) 2015年 hehehhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiDuMap/BaiDuMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiDuMap
+//
+//  Created by lanou3g on 15-7-25.
+//  Copyright (c) 2015年 hehehhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
